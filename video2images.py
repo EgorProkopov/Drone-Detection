dd36906781f5486de9b4e66e4f3f9785f521a775
EgorProkopov <egorprokopov216@gmail.com>
2022-11-21 21:35:59 +0300

    поправил video2images файл

--- video2images.py
+++ video2images.py
@@ -1,23 +1,30 @@
 import cv2
 
 
-if __name__ == "__main__":
-    cap = cv2.VideoCapture('video/video3.mp4')
+def video2images(video_name, frame_num):
+    cap = cv2.VideoCapture(f'videos/{video_name}.mp4')
 
-    frameCounter = 0
+    frame_counter = 0
     while True:
         ret, frame = cap.read()
         cv2.imshow("video3", frame)
 
-        if frameCounter % 10 == 0:
-            isWritten = cv2.imwrite(f'images/video3/video3-frame-{frameCounter}.png', frame)
+        if frame_counter % frame_num == 0:
+            isWritten = cv2.imwrite(f'images/{video_name}/{video_name}-frame-{frame_counter}.png', frame)
             if isWritten:
-                print(f"Frame {frameCounter} successfully saved to folder 'images'")
+                print(f"Frame {frame_counter} successfully saved to folder 'images'")
             else:
-                print(f"Failed to save frame {frameCounter}")
-                break;
+                print(f"Failed to save frame {frame_counter}")
+                break
 
-        frameCounter += 1
+        frame_counter += 1
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
+
+if __name__ == "__main__":
+    video_names = ["video3"]
+    frame_num = 20
+
+    for video_name in video_names:
+        video2images(video_name, frame_num)
